0b28bb7112b5ed7b20b12a168eb6a575b33a5b57
Jaid <jaid.jsx@gmail.com>
2023-02-11 18:15:00 +0100

    Simplified building
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/bin/installPackages.bash
+++ src/bin/installPackages.bash
@@ -2,8 +2,5 @@
 set -o errexit -o pipefail
 
 aptGet update
-aptGet upgrade
 aptGet install curl build-essential flex texinfo gettext automake pkg-config
-aptGet autoclean
-aptGet autoremove
 rm --recursive --force /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /usr/share/doc /usr/share/man
--- src/userBin/installCargoPackage.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o pipefail
-
-PATH=$PATH:$HOME/.cargo/bin
-export PATH
-rustTarget=$(rustc -vV | sed -n 's|host: ||p')
-cargo install --target "$rustTarget" --force "$@" --root /
