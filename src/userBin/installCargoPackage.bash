b29c1efbcc20c815d9413db371563d00d8f06e15
Jaid <jaid.jsx@gmail.com>
2023-05-23 19:14:44 +0200

    Updated changefile.txt

--- src/userBin/installCargoPackage.bash
+++ src/userBin/installCargoPackage.bash
@@ -4,4 +4,4 @@ set -o errexit -o pipefail
 PATH=$PATH:$HOME/.cargo/bin
 export PATH
 rustTarget=$(rustc -vV | sed -n 's|host: ||p')
-cargo install --target "$rustTarget" --force "$@" --root /
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo install --target "$rustTarget" --force "$@" --root /
