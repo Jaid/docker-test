e5733e402882a9caacb7ee8795da1f002951313a
Jaid <6216144+Jaid@users.noreply.github.com>
2023-07-21 20:24:04 +0200

    Updated changefile.txt

--- src/run.bash
+++ src/run.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail -o xtrace
 
-hyperfine --style basic --export-markdown hyperfine.md --output ./pyramid.txt --shell none 'smileypyramid 10' &
+if [[ $base = a ]]; then
+  pyramidSize=10
+else
+  pyramidSize=20
+fi
+hyperfine --style basic --export-markdown hyperfine.md --output ./pyramid.txt --shell none smileypyramid $pyramidSize &
 pid=$!
 top -b -n 1
 wait $pid
