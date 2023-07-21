7381031195bd4dafc203ed1a8545e0483a41cf0b
Jaid <6216144+Jaid@users.noreply.github.com>
2023-07-21 20:15:52 +0200

    Updated changefile.txt

--- src/run.bash
+++ src/run.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail -o xtrace
 
-tini -s -- hyperfine --style basic --export-markdown hyperfine.md --output ./pyramid.txt --shell none 'smileypyramid 10' &
+hyperfine --style basic --export-markdown hyperfine.md --output ./pyramid.txt --shell none 'smileypyramid 10' &
 pid=$!
 top -b -n 1
 wait $pid
